f0bc670dfb70fc25e8b0ed7882a4c5de071f0d4a
Aleksander <asava@sibmail.com>
2023-06-21 14:44:00 +0700

    ok

--- client/src/pages/Categories.tsx
+++ client/src/pages/Categories.tsx
@@ -1,7 +1,29 @@
 import { FC } from 'react'
+import { AiFillCloseCircle, AiFillEdit } from 'react-icons/ai'
+import { Form } from 'react-router-dom'
 
 const Categories: FC = () => {
-  return <div>Categories</div>
+	return (
+		<div className=" mt-10 rounded-md bg-slate-800 p-4">
+			<h1>You category list</h1>
+			<div className="mt-2 flex flex-wrap items-center gap-2">
+				<div className=" group relative flex items-center gap-2 rounded-lg bg-blue-800 px-4 py-2">
+					Salry
+					<div className=" absolute bottom-0 left-0 right-0 top-0 hidden items-center justify-between rounded-lg bg-black/90 px-3 group-hover:flex">
+						<button>
+							<AiFillEdit />
+						</button>
+						<Form className=" flex" method="delete" action="/categories">
+							<input type="hidden" value={'Category ID'} />
+							<button type="submit">
+								<AiFillCloseCircle />
+							</button>
+						</Form>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Categories
